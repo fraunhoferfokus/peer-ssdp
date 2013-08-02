146a4e3995010471c83f818239957567d3a06572
Martin Lasak <martin.lasak@fokus.fraunhofer.de>
2013-08-02 12:50:53 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,87 @@
+peer-ssdp 
+=========
+
+peer-ssdp is a simple Node.js module implementing the Simple Service Discovery Protocol SSDP as described in the
+[UPnP Device Architecture specification, Section 1](http://www.upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.1.pdf)
+
+Setup
+=====
+
+  * use `npm install peer-ssdp` to install the module.
+  
+Usage
+=====
+Peer is an `EventEmitter` so you can use the common `EventEmitter` API to subscribe to specific events.
+
+```javascript
+var ssdp = require("peer-ssdp");
+var peer = ssdp.createPeer();
+var interval;
+/**
+ * handle peer ready event. This event will be emitted after `peer.start()` is called.
+ */
+peer.on("ready",function(){
+	// handle ready event
+	// send ssdp:alive messages every 1s
+	interval = setInterval(function(){
+		peer.alive({
+			ST: "upnp:rootdevice",
+			SERVER: "...",
+			ST: headers.ST,
+			USN: "..."
+		});
+	}, 1000);
+	// shutdown peer after 10 s and send a ssdp:byebye message before
+	setTimeout(function(){
+		clearInterval(interval);
+		// Close peer. Afer peer is closed the `close` event will be emitted.
+		peer.close();
+	}, 10000);
+});
+
+// handle SSDP NOTIFY messages. 
+// param headers is JSON object containing the headers of the SSDP NOTIFY message as key-value-pair. 
+// param address is the socket address of the sender
+peer.on("notify",function(headers, address){
+	// handle notify event
+});
+
+// handle SSDP M-SEARCH messages. 
+// param headers is JSON object containing the headers of the SSDP M-SEARCH message as key-value-pair. 
+// param address is the socket address of the sender
+peer.on("search",function(headers, address){
+	//handle search request
+	// reply to search request 
+	peer.reply({
+		ST: "upnp:rootdevice",
+		SERVER: "...",
+		ST: headers.ST,
+		USN: "..."
+	},address);
+});
+
+// handle SSDP HTTP 200 OK messages. 
+// param headers is JSON object containing the headers of the SSDP HTTP 200 OK  message as key-value-pair. 
+// param address is the socket address of the sender
+peer.on("found",function(headers, address){
+	// handle found event
+});
+
+// handle peer close event. This event will be emitted after `peer.close()` is called.
+peer.on("close",function(){
+	// handle close event
+});
+
+// Start peer. Afer peer is ready the `ready` event will be emitted.
+peer.start();
+``` 
+
+License
+=======
+
+Free for non commercial use released under the GNU Lesser General Public License v3.0
+See LICENSE file.
+
+Contact us for commecial use famecontact@fokus.fraunhofer.de
+
+Copyright (c) 2013 Fraunhofer FOKUS
---
+++ test/ssdp-test.js
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * 
+ * Copyright (c) 2013 Louay Bassbouss, Fraunhofer FOKUS, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * AUTHORS: Louay Bassbouss (louay.bassbouss@fokus.fraunhofer.de)
+ *     Martin Lasak (martin.lasak@fokus.fraunhofer.de)
+ *     Alexander Futasz (alexander.futasz@fokus.fraunhofer.de)
+ *
+ ******************************************************************************/
+
+var os = require('os');
+var ssdp = require("peer-ssdp");
+var SERVER = os.type() + "/" + os.release() + " UPnP/1.1 famium/0.0.1";
+var uuid = "6bd5eabd-b7c8-4f7b-ae6c-a30ccdeb5988";
+var peer = ssdp.createPeer();
+
+peer.on("ready", function () {
+    console.log("ready");
+    onReady();
+}).on("notify", function (headers, address) {
+    console.log("receive notify message from ", address);
+    console.log(headers);
+    console.log("=======================");
+}).on("search", function (headers, address) {
+    console.log("receive search request message from ", address);
+    console.log(headers);
+    console.log("=======================");
+    var ST = headers.ST;
+    var headers = {
+        LOCATION: "http://0.0.0.0/upnp/devices/6bd5eabd-b7c8-4f7b-ae6c-a30ccdeb5988/desc.xml",
+        SERVER: SERVER,
+        ST: "upnp:rootdevice",
+        USN: "uuid:" + uuid + "::upnp:rootdevice",
+            'BOOTID.UPNP.ORG': 1
+    };
+    console.log("reply to search request from ", address);
+    console.log(headers);
+    console.log("=======================");
+    peer.reply(headers, address);
+}).on("found", function (headers, address) {
+    console.log("receive found message from ", address);
+    console.log(headers);
+    console.log("=======================");
+}).on("close", function () {
+    console.log("close");
+}).start();
+
+var onReady = function () {
+    console.log("notify SSDP alive message");
+    peer.alive({
+        NT: "upnp:rootdevice",
+        USN: "uuid:" + uuid + "::upnp:rootdevice",
+        LOCATION: "http://0.0.0.0/upnp/devices/6bd5eabd-b7c8-4f7b-ae6c-a30ccdeb5988/desc.xml",
+        SERVER: SERVER
+    });
+
+    console.log("search for rootdevices");
+    peer.search({
+        ST: "upnp:rootdevice"
+    });
+
+    setTimeout(function () {
+        console.log("notify SSDP byebye message");
+        peer.byebye({
+            NT: "upnp:rootdevice",
+            USN: "uuid:" + uuid + "::upnp:rootdevice",
+            LOCATION: "http://0.0.0.0/upnp/devices/6bd5eabd-b7c8-4f7b-ae6c-a30ccdeb5988/desc.xml",
+            SERVER: SERVER
+        }, function () {
+            peer.close();
+        });
+    }, 10000);
+};
